8963a8d8dceff8d67d4387c2025d66be13735aa2
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-05-06 18:14:52 +0200

    first commit

---
+++ social-icon.js
@@ -0,0 +1,9 @@
+import { LitElement, html, css } from 'lit-element';
+
+class SocialIcon extends LitElement {
+
+  render() {
+    return html``;
+  }
+}
+customElements.define('social-icon', SocialIcon);
